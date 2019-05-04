1c473e0e083e07cbf58848f2ae793a8449651ce7
Paweł Koniarski <pawel@koniar.ski>
2019-05-04 23:23:07 +0200

    Add scoring and game restart

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,6 +4,18 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+
+    private GameController _gameController;
+
+    private void Start()
+    {
+        var gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            _gameController = gameControllerObject.GetComponent<GameController>();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,8 +30,10 @@ public class DestroyByContact : MonoBehaviour
         {
             var playerTransform = other.transform;
             Instantiate(playerExplosion, playerTransform.position, playerTransform.rotation);
+            _gameController.GameOver();
         }
 
+        _gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -10,9 +12,34 @@ public class GameController : MonoBehaviour
     public float startWait;
     public float waveWait;
 
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool _gameOver;
+    private bool _restart;
+    private int _score;
+
     private void Start()
     {
+        _gameOver = false;
+        _restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        _score = 0;
+        UpdateScore();
         StartCoroutine(SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if (_restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
     }
 
     private IEnumerator SpawnWaves()
@@ -30,6 +57,30 @@ public class GameController : MonoBehaviour
             }
 
             yield return new WaitForSeconds(waveWait);
+
+            if (_gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                _restart = true;
+                break;
+            }
         }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        _score += newScoreValue;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = "Score: " + _score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        _gameOver = true;
     }
 }
