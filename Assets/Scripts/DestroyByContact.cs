760f097c8087ed78bc429718d9839f38f9188c02
Paweł Koniarski <pawel@koniar.ski>
2019-05-04 19:50:21 +0200

    Add explosions and make asteroid a prefab

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,11 +2,22 @@
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Boundary"))
         {
             return;
+        }
+
+        var thisTransform = transform;
+        Instantiate(explosion, thisTransform.position, thisTransform.rotation);
+        if (other.CompareTag("Player"))
+        {
+            var playerTransform = other.transform;
+            Instantiate(playerExplosion, playerTransform.position, playerTransform.rotation);
         }
 
         Destroy(other.gameObject);
