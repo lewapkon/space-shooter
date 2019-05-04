25508aff0e3a4b26f611ca1aade95350fa264637
Paweł Koniarski <pawel@koniar.ski>
2019-05-04 19:03:14 +0200

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.velocity = transform.forward * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float _nextFire;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > _nextFire)
+        {
+            _nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        _rb.velocity = new Vector3(moveHorizontal, 0, moveVertical) * speed;
+        _rb.position = new Vector3(
+            Mathf.Clamp(_rb.position.x, boundary.xMin, boundary.xMax),
+            0,
+            Mathf.Clamp(_rb.position.z, boundary.zMin, boundary.zMax));
+
+        _rb.rotation = Quaternion.Euler(0, 0, _rb.velocity.x * -tilt);
+    }
+}
