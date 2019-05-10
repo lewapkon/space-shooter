382ef52169b861addf24b14d8f80f714e9bab23f
Paweł Koniarski <pawel@koniar.ski>
2019-05-10 22:57:48 +0200

    Add two more types of asteroids

---
+++ Assets/Scripts/ScrollMaterial.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ScrollMaterial : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer _renderer;
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void Update()
+    {
+        var offset = Time.time * scrollSpeed;
+        _renderer.material.SetTextureOffset(MainTex, new Vector2(0, offset));
+    }
+}
