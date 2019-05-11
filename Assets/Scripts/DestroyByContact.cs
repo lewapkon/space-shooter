3cf25da1a7072f721ccc094d7c04e40c75513b05
Paweł Koniarski <pawel@koniar.ski>
2019-05-11 03:15:03 +0200

    Add enemies

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -19,21 +19,28 @@ public class DestroyByContact : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Boundary"))
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
         {
             return;
         }
 
-        var thisTransform = transform;
-        Instantiate(explosion, thisTransform.position, thisTransform.rotation);
+        if (explosion != null)
+        {
+            var thisTransform = transform;
+            Instantiate(explosion, thisTransform.position, thisTransform.rotation);
+        }
+
         if (other.CompareTag("Player"))
         {
             var playerTransform = other.transform;
             Instantiate(playerExplosion, playerTransform.position, playerTransform.rotation);
             _gameController.GameOver();
         }
+        else
+        {
+            _gameController.AddScore(scoreValue);
+        }
 
-        _gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public Vector2 fireRate;
+    public float fireDelay;
+    public float lowestPointFire;
+
+    private float _nextFire;
+    private AudioSource _audioSource;
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+        _nextFire = Time.time + fireDelay;
+    }
+
+    void Update()
+    {
+        if (Time.time > _nextFire && shotSpawn.position.z > lowestPointFire)
+        {
+            _nextFire = Time.time + Random.Range(fireRate.x, fireRate.y);
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            _audioSource.Play();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector3(0, 0, -speed);
+        _rb.rotation = Quaternion.Euler(0, -180, _rb.velocity.x * -tilt);
+    }
+}
